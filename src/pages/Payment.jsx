1e14794a8dc5b5787fe7372f37f11766fd18314b
heba essam <heba.work.2018@gmail.com>
2026-06-15 19:56:15 +0300

    update path

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,5 +1,6 @@
-import PaymentPage from '../components/payment/PaymentPage/PaymentPage';
+// import PaymentPage from '../components/payment/PaymentPage/PaymentPage';
 
+import PaymentPage from "../components/Payment/PaymentPage/PaymentPage";
 export default function Payment() {
   return <PaymentPage />;
 }
